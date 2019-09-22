79ace79d4ce8c687bb0bd547fbd2342a8b8ad735
zencloud <mryanmill@gmail.com>
2019-09-22 00:31:47 -0600

    Dev

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,39 @@
 
 $.getJSON("/articles", function (packageData) {
     packageData.forEach( (articleData) => {
-        console.log(articleData);
-
         let myHTML = `
             <div>
                 <p>${articleData.title}</p>
                 <button onclick="app_article_save('${articleData._id}')">Save</button>
+                <button>Comment</button>
             </div>`;
         $('#article-list').append(myHTML);
     });
 });
 
 
+$.getJSON("/saved", function (packageData) {
+    packageData.forEach( (articleData) => {
+        let myHTML = `
+            <div>
+                <p>${articleData.title}</p>
+                <button onclick="app_article_save('${articleData._id}')">Delete</button>
+                <button>Comment</button> 
+            </div>`;
+        $('#saved-list').append(myHTML);
+    });
+});
+
+$(document).ready(function() {
+    $('#toggle-saved').click(function() {
+        $("#saved-list").slideToggle(300);
+        $(this).text(function(i, text) {
+            return text === "Hide Saved" ? "Show Saved" : "Hide Saved";
+        });
+    });
+})
 
 function app_article_save(id) {
-
-    console.log(id);
     $.ajax({
         method: "POST",
         url: `/save/${id}`
